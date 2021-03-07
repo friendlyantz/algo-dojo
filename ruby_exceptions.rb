e5e5b9321490c8286e4ad9e3640ae9f3459ef8ab
Anton <panteleev23@gmail.com>
2021-03-08 09:52:36 +1100

    raising errors. also theory Re throw/catch VS raise,fail,rescue,ensure

---
+++ ruby_exceptions.rb
@@ -0,0 +1,11 @@
+# catch/throw are NOT the same as raise/rescue.
+# --raise, fail, rescue, and ensure handle errors/exceptions
+# --throw and catch are control flow
+# Unlike in other languages, Ruby’s throw and catch are not used for exceptions. Instead, they provide a way to terminate execution early when no further work is needed. (Grimm, 2011)
+# While the exception mechanism of raise and rescue is great for abandoning execution when things go wrong, it’s sometimes nice to be able to jump out of some deeply nested construct during normal processing. This is where catch and throw come in handy. (Thomas and Hunt, 2001)
+def power(n, p)
+  # begin
+  raise RangeError, 'n and p should be non-negative' if (n | p).negative?
+
+  n**p
+end
