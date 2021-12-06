c37593ed1b8818593c3bb6479e05af32ac76939a
Anton Panteleev <panteleev23@gmail.com>
2021-12-06 20:11:06 +1100

    DAY4p1: WIP. TODO: fix double up loop bingo check

--- advent_of_code/d04/04p1_bingo.rb
+++ advent_of_code/d04/04p1_bingo.rb
@@ -17,12 +17,48 @@ class Card
     @col_sums = [0, 0, 0, 0, 0]
   end
 
-  def bingo
-    puts "Bingo!!! Card with #{id}"
+  def cross_nums_in_card(magic_num)
+    card_nums.each do |card_num|
+      if card_num.num == magic_num
+        p card_num
+        p "xxxxxx ---> crossing #{magic_num}" if card_num.cross
+      end
+    end
+    self
   end
 
-  def bingo_check(_magic_number)
-    p 'checking....'
+  def bingo_check(magic_num)
+    if line_sums.include?(5) || col_sums.include?(5)
+      puts '________________________________'
+      puts "bingo!!! YOLOOO. card# #{id} won. magic num was: #{magic_num}"
+      puts '________________________________'
+    end
+  end
+
+  def update_line_and_col_sum_trackers(_magic_number)
+    5.times do |x|
+      5.times do |y|
+        this_loop_card_num = card_nums.select do |element|
+          element.x == x && element.y == y && element.crossed
+        end
+
+        next if this_loop_card_num.empty?
+
+        # puts "looping x: #{x}, y: #{y}"
+
+        # binding.pry
+
+        # TODO: double up calc issue
+        # binding.pry
+        line_sums[x] += 1
+        col_sums[y] += 1
+      end
+    end
+
+    # binding.pry
+    p line_sums
+    p col_sums
+    # break
   end
 end
 
@@ -64,52 +100,26 @@ def merge_nums_to_cards(data)
   cards
 end
 
+def assess_cards(array_of_cards, magic_num)
+  array_of_cards.each do |card|
+    p "assessing card: #{card.id}"
+    card.cross_nums_in_card(magic_num)
+
+    card.update_line_and_col_sum_trackers(magic_num)
+
+    card.bingo_check(magic_num)
+  end
+end
+
 def game_main_sequence(array_of_cards, drawing_numbers)
   drawing_numbers.each do |drawed_number|
+    puts '~~~~~~~~~~~~~~~~~~~~~~~~'
     puts "rolling #{drawed_number}"
 
     assess_cards(array_of_cards, drawed_number)
   end
 end
 
-def assess_cards(array_of_cards, magic_num)
-  array_of_cards.each do |card|
-    p "assessing card: #{card.id}"
-    crossed_card = cross_nums_in_card(card, magic_num)
-
-    5.times do |x|
-      5.times do |y|
-        this_loop_card_num = crossed_card.card_nums.select do |element|
-          element.x == x && element.y == y && element.crossed
-        end
-        this_loop_card_num = this_loop_card_num.first.num unless this_loop_card_num.empty?
-
-        card.line_sums[x] += 1
-        card.col_sums[y] += 1
-      end
-    end
-
-    # binding.pry
-
-    unless card.col_sums.select { |e| e == 5 }.empty? || card.line_sums.select { |e| e == 5 }.empty?
-      puts "bingo!!! YOLOOO. card# #{card.id} won"
-      break
-    end
-  end
-end
-
-def cross_nums_in_card(card, magic_num)
-  card.card_nums.each do |card_num|
-    card_num.cross if card_num.num == magic_num
-  end
-  card
-end
-
-def bingo_check; end
-
 cards = merge_nums_to_cards(card_raw_data)
 
-drawing_numbers.each do |magic_num|
-  p magic_num
-  assess_cards(cards, magic_num)
-end
+game_main_sequence(cards, drawing_numbers)
