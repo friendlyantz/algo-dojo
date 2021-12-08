887ba8761284fc8131603df0c923393fa2e4c071
Anton Panteleev <panteleev23@gmail.com>
2021-12-08 15:23:02 +1100

    DAY7p1: solved. ez

--- advent_of_code/d07/07_whales.rb
+++ advent_of_code/d07/07_whales.rb
@@ -1,4 +1,21 @@
-require_relative '06_data'
+require_relative '07_data'
 require 'pry'
 require 'awesome_print'
 
+def main_sequence(crabs)
+  table_of_fuel_tests = []
+  crabs.length.times do |i|
+    p "--------------------running test # #{i+1}--------------------"
+    fuel_consumed_this_test = 0
+    crabs.each do |crab|
+      diff = [crab, i].max - [crab, i].min
+      puts "Move from #{crab} to #{i}. cost of fuel: #{diff}"
+      fuel_consumed_this_test += diff
+      ap "fuel_consumed_this_test: #{fuel_consumed_this_test}"
+    end 
+    table_of_fuel_tests << fuel_consumed_this_test
+  end
+  ap table_of_fuel_tests.min
+end
+
+main_sequence(BIN_DATA)
