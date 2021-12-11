f9b6a551003858f99caff9fc5b72e009879b932f
Anton Panteleev <panteleev23@gmail.com>
2021-12-11 22:12:06 +1100

    TDD: find low points of line

--- advent_of_code/spec/d09_spec.rb
+++ advent_of_code/spec/d09_spec.rb
@@ -23,17 +23,27 @@ RSpec.describe 'Day 9 Smoke' do
   end
 
   describe 'low points' do
-    let(:array_of_lines) { [2_199_943_210,
-                       3_987_894_921,
-                       9_856_789_892,
-                       8_767_896_789,
-                       9_899_965_678] } 
+    let(:array_of_lines) do
+      [2_199_943_210,
+       3_987_894_921,
+       9_856_789_892,
+       8_767_896_789,
+       9_899_965_678]
+    end
     subject do
       find_low_points(array_of_lines)
     end
 
     it 'find_low_points outputs array of low point values' do
       expect(subject).to eq [1, 0, 5, 5]
+    end
+
+    it 'find_low_points_of_line outputs array of low point values' do
+      expect(find_low_points_of_line(2_199_943_210)).to eq [1, 0]
+      expect(find_low_points_of_line(3_987_894_921)).to eq []
+      expect(find_low_points_of_line(9_856_789_892)).to eq [5]
+      expect(find_low_points_of_line(8_767_896_789)).to eq []
+      expect(find_low_points_of_line(9_899_965_678)).to eq [5]
     end
 
     it 'calculates the sum of risk factors of low points' do
