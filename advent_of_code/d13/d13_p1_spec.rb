33004971a5c8c3f813f2aebc4a1063c8be4797ce
Anton Panteleev <panteleev23@gmail.com>
2022-09-20 22:19:33 +1000

    Day13p1 TDD prep

---
+++ advent_of_code/d13/d13_p1_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'd13_p1'
+
+RSpec.describe 'Day 13 part 1' do
+  describe 'solution' do
+    it 'returns a correct anwser' do
+      expect(TransparentOrigami.new.solution).to eq 'hey'
+    end
+  end
+end
