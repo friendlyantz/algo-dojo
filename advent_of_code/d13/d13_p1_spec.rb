283569a24aeddbc7a3590002c7b47dcc3afe14fb
Anton Panteleev <panteleev23@gmail.com>
2022-09-21 23:06:57 +1000

    TDD part_one solution

--- advent_of_code/d13/d13_p1_spec.rb
+++ advent_of_code/d13/d13_p1_spec.rb
@@ -30,32 +30,37 @@ RSpec.describe 'Day 13 part 1' do # rubocop:disable Metrics/BlockLength
   let(:origami) { TransparentOrigami.new(TEST_DATA) }
   let(:matrix) do
     "...#..#..#.
-      ....#......
-      ...........
-      #..........
-      ...#....#.#
-      ...........
-      ...........
-      ...........
-      ...........
-      ...........
-      .#....#.##.
-      ....#......
-      ......#...#
-      #..........
-      #.#........"
+     ....#......
+     ...........
+     #..........
+     ...#....#.#
+     ...........
+     ...........
+     ...........
+     ...........
+     ...........
+     .#....#.##.
+     ....#......
+     ......#...#
+     #..........
+     #.#........"
       .split.map(&:chars)
   end
-
+  let(:folded_origami) do
+    "#####
+         #...#
+         #...#
+         #...#
+         #####
+         .....
+         ....."
+      .split.map(&:chars)
+  end
   describe 'object initialization' do
     it 'reads data and formats it' do
       expect(origami.instructions).to eq ['fold along y=7', 'fold along x=5']
       expect(origami.coordinates).to eq [[6, 10], [0, 14], [9, 10], [0, 3], [10, 4], [4, 11], [6, 0], [6, 12], [4, 1],
                                          [0, 13], [10, 12], [3, 4], [3, 0], [8, 4], [1, 10], [2, 14], [8, 10], [9, 0]]
-    end
-
-    it 'returns a correct anwser' do
-      expect(origami.solution).to eq 'hey'
     end
   end
 
@@ -73,7 +78,7 @@ RSpec.describe 'Day 13 part 1' do # rubocop:disable Metrics/BlockLength
     end
   end
 
-  describe 'folding' do # rubocop:disable Metrics/BlockLength
+  describe 'folding' do
     context 'only 1 fold' do
       before do
         origami.instructions.pop
@@ -94,18 +99,34 @@ RSpec.describe 'Day 13 part 1' do # rubocop:disable Metrics/BlockLength
     end
 
     context 'all instruction folds' do
-      let(:expected) do
-        "#####
-         #...#
-         #...#
-         #...#
-         #####
-         .....
-         ....."
+      it 'folds matrix as per instructions' do
+        expect(origami.fold(matrix)).to eq folded_origami
+      end
+    end
+  end
+
+  describe 'count dots' do
+    context 'only after first fold' do
+      let(:staged_origami) do
+        "#.##..#..#.
+         #...#......
+         ......#...#
+         #...#......
+         .#.#..#.###
+         ...........
+         ..........."
           .split.map(&:chars)
       end
-      it 'folds matrix as per instructions' do
-        expect(origami.fold(matrix)).to eq expected
+      it 'counts total number of dots after folwing' do
+        expect(
+          origami.count_dots(staged_origami)
+        ).to eq 17
+      end
+
+      it '.solve_puzzle solves the 1st part of the puzzle all at one' do
+        expect(
+          origami.solve_puzzle_one
+        ).to eq 17
       end
     end
   end
