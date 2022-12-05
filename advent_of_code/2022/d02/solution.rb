d3c044707b21bd9b5c5a5cf35576cf51ab43c176
Anton Panteleev <friendlyantz@pm.me>
2022-12-05 19:54:22 +1100

    pt1 individual round outcome calc and rules defined

--- advent_of_code/2022/d02/solution.rb
+++ advent_of_code/2022/d02/solution.rb
@@ -10,10 +10,41 @@ OUTCOME_WEIGHT = {
   win: 6
 }
 
+OPPONENT_HAND_MAPPING = {
+  'A' => :rock,
+  'B' => :paper,
+  'C' => :scissors
+}
+
+OUR_HAND_MAPPING = {
+  'X' => :rock,
+  'Y' => :paper,
+  'Z' => :scissors
+}
+
+RULES = {
+  rock:  {
+    rock: :draw,
+    paper: :loss,
+    scissors: :win
+  },
+  paper: {
+    rock: :win,
+    paper: :draw,
+    scissors: :loss
+  },
+  scissors: {
+    rock: :loss,
+    paper: :win,
+    scissors: :draw
+  }
+}
+
 def solution_pt1(input)
   input
     .then { |data| prep(data) }
-    # .then { |data| binding.pry }
+    .map { |data| calc_round_outcome(data) }
+  # .then { |data| binding.pry }
 end
 
 def solution_pt2(_input)
@@ -21,5 +52,17 @@ def solution_pt2(_input)
 end
 
 def prep(data)
-  data.lines.map(&:strip)
+  data.lines
+      .map(&:strip)
+      .map(&:split)
+end
+
+def calc_round_outcome(data)
+   SHAPE_WEIGHT[OUR_HAND_MAPPING[data.last]] + OUTCOME_WEIGHT[win_loss_or_draw?(data)]
+end
+
+def win_loss_or_draw?(data)
+  opponent_hand = OPPONENT_HAND_MAPPING[data.first]
+  our_hand =  OUR_HAND_MAPPING[data.last]
+  RULES[our_hand][opponent_hand]
 end
