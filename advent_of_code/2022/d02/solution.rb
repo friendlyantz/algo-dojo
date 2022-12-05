9b73adf7b0e8c060c6eb9fb7d1e984edf7ec131d
Anton Panteleev <friendlyantz@pm.me>
2022-12-05 19:12:58 +1100

    pt1 data prep

--- advent_of_code/2022/d02/solution.rb
+++ advent_of_code/2022/d02/solution.rb
@@ -1,7 +1,25 @@
+SHAPE_WEIGHT = {
+  rock: 1,
+  paper: 2,
+  scissors: 3
+}
+
+OUTCOME_WEIGHT = {
+  loss: 0,
+  draw: 3,
+  win: 6
+}
+
 def solution_pt1(input)
   input
+    .then { |data| prep(data) }
+    # .then { |data| binding.pry }
 end
 
 def solution_pt2(_input)
   'implementation'
+end
+
+def prep(data)
+  data.lines.map(&:strip)
 end
