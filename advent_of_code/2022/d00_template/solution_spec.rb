d9984de0196050b81fb2cf722a29623940a34cf9
Anton Panteleev <friendlyantz@pm.me>
2022-12-05 19:02:42 +1100

    Fix template typo

--- advent_of_code/2022/d00_template/solution_spec.rb
+++ advent_of_code/2022/d00_template/solution_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe 'Solutions' do
     describe 'final result for pt1' do
       context 'example input data' do
         it 'return correct result' do
-          expect(solution_pt2(example_input)).to eq 'sample_input'
+          expect(solution_pt1(example_input)).to eq 'sample_input'
         end
       end
 
       context 'custom input data' do
         it 'return correct result' do
           pending 'pt1 implementation'
-          expect(solution_pt2(custom_input)).to eq 'test input'
+          expect(solution_pt1(custom_input)).to eq 'test input'
         end
       end
     end
