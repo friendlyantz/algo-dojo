050bcb134f3ba1c50c71fb8537161dd8dabd1996
Anton Panteleev <friendlyantz@pm.me>
2022-12-12 06:30:58 +1100

    AoC22 d09 task added

---
+++ advent_of_code/2022/d09/solution.rb
@@ -0,0 +1,26 @@
+require File.join(__dir__, '../lib/my_colorize')
+
+include MyColorize
+
+def solution_pt1(input)
+  input
+end
+
+def solution_pt2(input)
+  # input
+  # .then { |data| binding.pry }
+end
+
+if __FILE__ == $PROGRAM_NAME
+  if ARGV.empty?
+    raise 'please provide input file destination as an argument following script name when executing this file'
+  end
+
+  input = File.read(ARGV.first)
+  puts 'part 1 solution'
+  puts solution_pt1(input)
+
+  puts '==============='
+  puts 'part 2 solution'
+  puts solution_pt2(input)
+end
