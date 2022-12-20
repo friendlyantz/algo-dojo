bd2a49f3d8f93fb164a1ac14b3f251f57a512e4f
Anton Panteleev <friendlyantz@pm.me>
2022-12-21 08:29:10 +1100

    Init monkey with params

--- advent_of_code/2022/d11/solution.rb
+++ advent_of_code/2022/d11/solution.rb
@@ -1,5 +1,9 @@
+require 'json'
+
 def solution_pt1(input)
   input
+    .then { |data| prep_data(data) }
+    .then { |data| genrate_moneys_from(data) }
 end
 
 def solution_pt2(input)
@@ -8,8 +12,56 @@ def solution_pt2(input)
 end
 
 class Monkey
-  
-  
+  attr_reader :id, :items, :divisible_by
+  attr_accessor :if_true, :if_false
+
+  def initialize(id:, items:, operation:, divisible_by:, if_true:, if_false:)
+    @id = id
+    @items = items
+    @operation = operation
+    @divisible_by = divisible_by
+    @if_true = if_true
+    @if_false = if_false
+  end
+end
+
+def prep_data(data)
+  data.split("\n\n").map { _1.lines.map(&:chomp) }
+end
+
+def monkeys
+  @monkeys ||= []
+end
+
+def genrate_moneys_from(data)
+  data.map.with_index do |group, i|
+    monkeys << Monkey.new(
+      id: i,
+      items: group[1].scan(/\d+/).map(&:to_i),
+      operation: interpret(group[2]),
+      divisible_by: group[3].scan(/\d+/).first.to_i,
+      if_true: group[4].scan(/\d+/).first.to_i,
+      if_false: group[5].scan(/\d+/).first.to_i
+    )
+  end
+
+  map_monkey_to_monkey(monkeys)
+end
+
+def interpret(operation)
+  operation
+    .scan(/(old|[+|*|-|\/]|\d+)/)
+    .flatten
+
+    # binding.pry
+    # TODO
+end
+
+def map_monkey_to_monkey(monkeys)
+  monkeys.each_with_index do |monkey, _i|
+    monkey.if_true = monkeys.find { _1.id == monkey.if_true }
+    monkey.if_false = monkeys.find { _1.id == monkey.if_false }
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
