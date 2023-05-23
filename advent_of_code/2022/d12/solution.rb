742d52bf87890349019b50b48acbd874a2b9a54c
Anton Panteleev <friendlyantz@pm.me>
2023-05-23 10:59:48 +1000

    efac

--- advent_of_code/2022/d12/solution.rb
+++ advent_of_code/2022/d12/solution.rb
@@ -6,75 +6,86 @@ require File.join(__dir__, '../lib/neighbour_yield_lateral')
 require File.join(__dir__, '../lib/visualisation.rb')
 
 class SolutionOne
-  def run(input, _)
-    topo_map = []
-    grid = []
-    input.each_line do |line|
-      topo_map <<
-        line.chars.map do |x|
+  attr_reader :start, :finish, :alt_map
+  attr_accessor :nav_map, :visited
+
+  def initialize(input)
+    @nav_map = [] # map with explored path
+    generate_nav_map(input)
+    @alt_map = [] # map with altitudes
+    generate_alt_map
+    @start = [0, 0]
+    @finish = [0, 0]
+    set_start_and_finish
+    @visited = {}
+  end
+
+  def generate_nav_map(input)
+    input.lines do |line|
+      nav_map << line.chomp.chars
+    end
+  end
+
+  def generate_alt_map
+    nav_map.each do |line|
+      alt_map <<
+        line.map do |x|
           case x
           when 'S' then 0
           when 'E' then 27
           else          x.ord - 96
           end
         end
-      grid << line.chars
     end
-    start_x = 0
-    start_y = 0
-    end_x = 0
-    end_y = 0
-    topo_map.each_with_index do |x, idx|
-      x.each_with_index do |y, idy|
-        if y == 27
-          end_x = idx
-          end_y = idy
-        end
-        if y == 0
-          start_x = idx
-          start_y = idy
-        end
-      end
-    end
-    visited = {}
-    step(start_x, start_y, topo_map, grid, 0, visited)
-    visited["#{end_x}:#{end_y}"]
   end
 
-  def step(x, y, map, grid, steps, visited)
+  def set_start_and_finish
+    alt_map.each_with_index do |x, idx|
+      x.each_with_index do |y, idy|
+        @finish = [idx, idy] if y == 27
+        @start = [idx, idy] if y == 0
+      end
+    end
+  end
+
+  def run
+    step(start[0], start[1], 0, visited)
+    visited["#{finish[0]}:#{finish[1]}"]
+  end
+
+  def step(x, y, steps, visited)
     return visited if !visited["#{x}:#{y}"].nil? && steps >= visited["#{x}:#{y}"]
 
     visited["#{x}:#{y}"] = steps
-    current = map[x][y]
+    current = alt_map[x][y]
     return visited if current == 27
 
-    grid[x][y] = '#'
-    print(grid)
+    nav_map[x][y] = '#'
+    print(nav_map)
 
-    up = (y - 1 >= 0 ? map[x][y - 1] : 30)
-    down = (y + 1 <= map.first.size - 1 ? map[x][y + 1] : 30)
-    left = (x - 1 >= 0 ? map[x - 1][y] : 30)
-    right = (x + 1 <= map.size - 1 ? map[x + 1][y] : 30)
+    up = (y - 1 >= 0 ? alt_map[x][y - 1] : 30)
+    down = (y + 1 <= alt_map.first.size - 1 ? alt_map[x][y + 1] : 30)
+    left = (x - 1 >= 0 ? alt_map[x - 1][y] : 30)
+    right = (x + 1 <= alt_map.size - 1 ? alt_map[x + 1][y] : 30)
 
-    visited = step(x, y - 1, map, grid, steps + 1, visited) if up - current < 2 || (current == 25 && up == 27)
-    visited = step(x, y + 1, map, grid, steps + 1, visited) if down - current < 2 || (current == 25 && down == 27)
-    visited = step(x - 1, y, map, grid, steps + 1, visited) if left - current < 2 || (current == 25 && left == 27)
-    visited = step(x + 1, y, map, grid, steps + 1, visited) if right - current < 2 || (current == 25 && right == 27)
+    visited = step(x, y - 1, steps + 1, visited) if up - current < 2 || (current == 25 && up == 27)
+    visited = step(x, y + 1, steps + 1, visited) if down - current < 2 || (current == 25 && down == 27)
+    visited = step(x - 1, y, steps + 1, visited) if left - current < 2 || (current == 25 && left == 27)
+    visited = step(x + 1, y, steps + 1, visited) if right - current < 2 || (current == 25 && right == 27)
 
-    grid[x][y] =
-      if map[x][y].zero?
+    nav_map[x][y] =
+      if alt_map[x][y].zero?
         'S'
       else
-        (map[x][y] + 96).chr
+        (alt_map[x][y] + 96).chr
       end
-    print(grid)
+    print(nav_map)
     visited
   end
-  
 
-  def print(grid)
+  def print(nav_map)
     Visualisation.print_grid(
-      grid,
+      nav_map,
       centre_x: 21, centre_y: 35, x_dim: 42, y_dim: 170,
       # sleep: 0.01,
       spacer: ' ',
@@ -84,7 +95,7 @@ class SolutionOne
 end
 
 def solution_pt1(input)
-  SolutionOne.new.run(input, nil)
+  SolutionOne.new(input).run
 end
 
 if __FILE__ == $PROGRAM_NAME
