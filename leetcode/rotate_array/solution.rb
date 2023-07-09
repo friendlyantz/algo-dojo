0540e427b8dc460dbef81099c7d81e87f97c013a
friendlyantz <friendlyantz@pm.me>
2023-07-09 10:51:42 +1000

    Array Rotation N(1)

---
+++ leetcode/rotate_array/solution.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+#
+# Given an integer nums nums, rotate the nums to the right by k steps, where k is non-negative.
+
+#Example 1:
+
+#Input: nums = [1,2,3,4,5,6,7], k = 3
+#Output: [5,6,7,1,2,3,4]
+#Explanation:
+#rotate 1 steps to the right: [7,1,2,3,4,5,6]
+#rotate 2 steps to the right: [6,7,1,2,3,4,5]
+#rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+#Example 2:
+
+#Input: nums = [-1,-100,3,99], k = 2
+#Output: [3,99,-1,-100]
+#Explanation: 
+#rotate 1 steps to the right: [99,-1,-100,3]
+#rotate 2 steps to the right: [3,99,-1,-100]
+
+#Constraints:
+
+#    1 <= nums.length <= 105
+#    -231 <= nums[i] <= 231 - 1
+#    0 <= k <= 105
+
+#Follow up:
+
+#    Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+#    Could you do it in-place with O(1) extra space?
+
+def solution(nums, k)
+  k.times do |i|
+    nums.unshift(nums.pop)
+  end
+
+  nums
+end
+
+puts solution([7, 1, 5, 3, 2, 6, 4])
+puts solution([1, 2, 3, 4, 5])
