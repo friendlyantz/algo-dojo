5f8f827b1d91f002622908badce0af57bbd9f694
friendlyantz <friendlyantz@pm.me>
2023-07-10 19:19:45 +1000

    LEET Sudoku solved, 1st attempt
    MEM could be optimised

--- leetcode/valid_sudoku/solution.rb
+++ leetcode/valid_sudoku/solution.rb
@@ -44,8 +44,52 @@
 #     board[i].length == 9
 #     board[i][j] is a digit 1-9 or '.'.
 
+def neighbour_yeild(data, line, column)
+  i, j = line, column
+  yield [i, j] # self
+  yield [i - 1, j - 1] if i > 0 && j > 0 # top-left
+  yield [i - 1, j] if i > 0 # top
+  yield [i - 1, j + 1] if i > 0 && j < data.first.length - 1 # top-right
+  yield [i + 1, j] if i < data.length - 1 # bottom
+  yield [i + 1, j - 1] if i < data.length - 1 && j > 0 # bottom-left
+  yield [i + 1, j + 1] if i < data.length - 1 && j < data.first.length - 1 # bottom-right
+  yield [i, j - 1] if j > 0 # left
+  yield [i, j + 1] if j < data.first.length - 1 # right
+end
+
 def is_valid_sudoku(board)
-  # TODO
+  # GOOD Runtime: 93 - 104 ms 66-92% better
+  # BAD Memory Usage: 211.1 -211.2 MB 0-45% better - Fluctuates, narrow band
+  # 1. check lines
+  board.each do |line|
+    line = line.dup
+    line.delete('.')
+    return false unless line.uniq == line
+  end
+
+  # 2. check columns
+  9.times do |column_index|
+    column = board.map { |line| line[column_index] }
+    column.delete('.')
+    return false unless column.uniq == column 
+  end
+
+  # 3. check 3x3 sub-boxes
+  3.times do |i|
+    3.times do |j|
+      stack = []
+      column = 1 + j*3
+      line = 1 + i*3
+      neighbour_yeild(board, line, column) do |dline, dcolumn|
+        stack << board[dline][dcolumn]
+      end
+
+      stack.delete('.')
+      return false unless stack.uniq == stack
+    end
+  end
+
+  return true
 end
 
 RSpec.describe 'Solution' do
