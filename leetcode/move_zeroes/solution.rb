70734e687565b1750749d44f597aff877a0cfe43
friendlyantz <friendlyantz@pm.me>
2023-07-10 13:33:57 +1000

    LEET solved using stop_index

--- leetcode/move_zeroes/solution.rb
+++ leetcode/move_zeroes/solution.rb
@@ -21,13 +21,36 @@
 
 # Follow up: Could you minimize the total number of operations done?
 
-def move_zeroes(nums); end
+def move_zeroes(nums)
+  # naive count 0, delete, push 0 * count.times
+  # MEMhog - doesnt even make to rankings on LEETCODE
+    # count = nums.count(0)
+    # nums.delete(0)
+    # nums + Array.new(count, 0)
+
+  # proper'ish solution. using delete_at and push
+  # delete_at magic shifts the tails, so we don't need to worry about it
+  # Runtime: 111-124-149 ms 20-80-(97)% better than other submissions UNSTABLE
+  # Memory Usage: 212.6-8 MB 90+% better than other submissions - STABLEish
+  stop_index = nums.size - 1
+  index = 0
+  until index == stop_index
+    if nums[index] == 0
+      nums.delete_at(index)
+      nums << 0
+      stop_index -= 1
+    else
+      index += 1
+    end
+  end
+  nums
+end
 
 RSpec.describe 'Solution' do
   let(:input_one) { [0, 1, 0, 3, 12] }
   let(:input_two) { [0] }
   it 'returns incremented big num as an array of digits' do
-    expect(move_zeroes(input_one)).to eq([1, 2, 4])
-    expect(move_zeroes(input_two)).to eq [1, 0, 0]
+    expect(move_zeroes(input_one)).to eq([1, 3, 12, 0, 0])
+    expect(move_zeroes(input_two)).to eq [0]
   end
 end
