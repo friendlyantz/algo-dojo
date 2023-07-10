8cd8fdcfa4eadbb43732f5b414ffd347f56c89e9
friendlyantz <friendlyantz@pm.me>
2023-07-10 10:07:13 +1000

    LEET solved using break, to avoid using 'each' loop

--- leetcode/plus_one/solution.rb
+++ leetcode/plus_one/solution.rb
@@ -4,8 +4,6 @@
 
 # Increment the large integer by one and return the resulting array of digits.
 
- 
-
 # Example 1:
 
 # Input: digits = [1,2,3]
@@ -30,8 +28,6 @@
 # Incrementing by one gives 9 + 1 = 10.
 # Thus, the result should be [1,0].
 
- 
-
 # Constraints:
 
 #     1 <= digits.length <= 100
@@ -39,14 +35,28 @@
 #     digits does not contain any leading 0's.
 
 def plus_one(digits)
-    
+  # reverse - reverse with break, to avoid looping through all elements
+  # LEETCODE stats: 80-85% better than other submissions TIME and MEM
+  # Runtime: 73 ms
+  # Memory Usage: 211 MB
+  digits.reverse!
+  digits.size.times do |i|
+    if (digits[i]) == 9
+      digits[i] = 0
+      digits << 1 if i == digits.size - 1
+    else
+      digits[i] += 1
+      break
+    end
+  end
+  digits.reverse!
 end
 
 RSpec.describe 'Solution' do
-  let(:input_one) { [1, 1, 1, 3, 3, 4, 3, 2, 4, 2] }
-  let(:input_two) { [1, 2, 3, 4] }
-  it 'return true for double up values and false if all unique' do
-    expect(contains_duplicate(input_one)).to eq true
-    expect(contains_duplicate(input_two)).to eq false
+  let(:input_one) { [1, 2, 3] }
+  let(:input_two) { [9, 9] }
+  it 'returns incremented big num as an array of digits' do
+    expect(plus_one(input_one)).to eq([1, 2, 4])
+    expect(plus_one(input_two)).to eq [1, 0, 0]
   end
 end
