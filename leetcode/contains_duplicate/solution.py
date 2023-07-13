c2d26db37002dfb958bd66ffc3512bc4190cb072
friendlyantz <friendlyantz@pm.me>
2023-07-13 13:40:04 +1000

    Python initial solution and test

---
+++ leetcode/contains_duplicate/solution.py
@@ -0,0 +1,29 @@
+import unittest
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        if len(nums) == 0:
+            return False
+        
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        
+        return False
+
+class TestSolution(unittest.TestCase):
+    def test_containsDuplicate_true(self):
+        s = Solution()
+        result = s.containsDuplicate([1, 2, 3, 1])
+        self.assertTrue(result)
+        
+    def test_containsDuplicate_false(self):
+        s = Solution()
+        result = s.containsDuplicate([1, 2, 3])
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
