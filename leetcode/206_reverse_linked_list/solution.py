d12e7209b1432f9fd8fef9b4f56a3a52124c7154
friendlyantz <friendlyantz@pm.me>
2023-07-14 16:59:31 +1000

    LEET: reverse NodeList using recursion

--- leetcode/206_reverse_linked_list/solution.py
+++ leetcode/206_reverse_linked_list/solution.py
@@ -33,25 +33,45 @@ class ListNode:
 import pdb
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        # Runtime # Details # 46ms # Beats 87.44%of users with Python3 
-        # Memory # Details # 17.71mb # Beats 98.35%of users with Python3
-        if head is None:
-            return None
+    def reverseList(self, head: ListNode, tail=None) -> ListNode:
+        # Runtime O(n) # Details # 46ms # Beats 87.44%of users with Python3 
+        # Memory O(1) # Details # 17.71mb # Beats 98.35%of users with Python3
 
-        # initial assignment
-        current_pointer = head
-        next_pointer = head.next
-        current_pointer.next = None
+        # if head is None:
+        #     return None
 
-        while next_pointer is not None:
-            # Buffer the next next pointer
-            next_next_pointer = next_pointer.next
+        # # # initial assignment
+        # current_pointer = head
+        # next_pointer = head.next
+        # current_pointer.next = None
 
-            # Reverse the pointer direction
-            next_pointer.next = current_pointer
+        # while next_pointer is not None:
+        #     # Buffer the next next pointer
+        #     next_next_pointer = next_pointer.next
 
-            # Move the pointers forward
-            current_pointer = next_pointer
-            next_pointer = next_next_pointer
-        return current_pointer
+        #     # Reverse the pointer direction
+        #     next_pointer.next = current_pointer
+
+        #     # Move the pointers forward
+        #     current_pointer = next_pointer
+        #     next_pointer = next_next_pointer
+        # return current_pointer
+        
+        # -----
+        # recursive implementation 
+        # -----
+        # Runtime 46ms Beats 87.44%of users with Python3
+        # Memory Details 17.71mb Beats 98.35%of users with Python3
+        def reverse_helper(current_head, new_head):
+            # PRE RECURSE - check before winding the stack
+            if current_head is None: # None, 5
+                # POST RECURSE - stack unwinding
+                return new_head
+
+            # RECURSE STAGE, I.E.[2, 3, 4, 5]
+            next_head = current_head.next # 3 | 4 | 5 | None
+            current_head.next = new_head # 2 -> None | 3 -> 2 | 4 -> 3 | 5 -> 4
+            return reverse_helper(next_head, current_head) # 3, 2 | 4, 3 | 5, 4 | None, 5
+
+        return reverse_helper(head, None)
+        
