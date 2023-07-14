e2dd3145633ef78cf5a68ce1049bd6bc47e86ac4
friendlyantz <friendlyantz@pm.me>
2023-07-14 11:41:52 +1000

    LEET: slow solution to binary search due to '!=' comparator

--- leetcode/704_binary_search/solution.py
+++ leetcode/704_binary_search/solution.py
@@ -25,7 +25,58 @@
 
 import unittest
 from typing import List
+import math # might be too slow for LEET
 
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        return True
+        # Output Limit Exceeded
+        head_i = 0
+        tail_i = len(nums) - 1
+        while head_i != tail_i:
+            # diff = math.floor((tail_i - head_i) / 2)
+            diff = math.floor((tail_i - head_i) / 2)
+            mid_i = head_i + diff
+
+            if nums[mid_i] == target:
+                return mid_i
+            elif nums[mid_i] < target:
+                head_i = mid_i + 1
+            elif nums[mid_i] > target:
+                tail_i = mid_i - 1
+
+        if nums[tail_i] == target:
+            return tail_i
+        else:
+            return -1
+
+
+class TestSolution(unittest.TestCase):
+    def test_search(self):
+        s = Solution()
+
+        # Test case 1
+        nums = [-1,0,3,5,9,12]
+        target = 9
+        expected_output = 4
+        self.assertEqual(s.search(nums, target), expected_output)
+
+        # # Test case 2
+        nums = [-1,0,3,5,9,12]
+        target = 2
+        expected_output = -1
+        self.assertEqual(s.search(nums, target), expected_output)
+
+        # # Test case 3
+        nums = [1,3,5,7,9]
+        target = 7
+        expected_output = 3
+        self.assertEqual(s.search(nums, target), expected_output)
+
+        # Test case 4
+        nums = [1,3,5,7,9]
+        target = 4
+        expected_output = -1
+        self.assertEqual(s.search(nums, target), expected_output)
+
+if __name__ == '__main__':
+    unittest.main()
