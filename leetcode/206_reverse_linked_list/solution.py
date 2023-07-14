d50992d46a1842a6cfae5ea3dd16114b84ce57d0
friendlyantz <friendlyantz@pm.me>
2023-07-14 13:22:57 +1000

    LEET: solved. removed broken tests

--- leetcode/206_reverse_linked_list/solution.py
+++ leetcode/206_reverse_linked_list/solution.py
@@ -26,6 +26,32 @@
 
 import unittest
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+import pdb
+
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return True
+    def reverseList(self, head: ListNode) -> ListNode:
+        # Runtime # Details # 46ms # Beats 87.44%of users with Python3 
+        # Memory # Details # 17.71mb # Beats 98.35%of users with Python3
+        if head is None:
+            return None
+
+        # initial assignment
+        current_pointer = head
+        next_pointer = head.next
+        current_pointer.next = None
+
+        while next_pointer is not None:
+            # Buffer the next next pointer
+            next_next_pointer = next_pointer.next
+
+            # Reverse the pointer direction
+            next_pointer.next = current_pointer
+
+            # Move the pointers forward
+            current_pointer = next_pointer
+            next_pointer = next_next_pointer
+        return current_pointer
