a9e6145218d6967c4f2e5b31b4e9aabb08009976
friendlyantz <friendlyantz@pm.me>
2023-07-15 12:26:54 +1000

    LEET: TODO: STUDY example, DEBUG my implementation

--- leetcode/110_balanced_binary_tree/solution.py
+++ leetcode/110_balanced_binary_tree/solution.py
@@ -32,7 +32,53 @@ def TreeNode(val=0, left=None, right=None):
 
 from typing import Optional
 import pdb
+import unittest
 
 class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        return True
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    #     print(f"node.val: #{root}")
+    #     def depth(node):
+    #         #PRE
+    #         if node == None:
+    #             return 0
+    #         #REC
+    #         print(f"node.val: {node.val}, l_depth: {l_depth}, r_depth: {r_depth}")
+    #         l_depth = depth(node.left) # returns int or False
+    #         r_depth = depth(node.right) # returns int or False
+
+    #         #POST
+    #         # circuit breakers
+    #         print(f"node.val: {node.val}, l_depth: {l_depth}, r_depth: {r_depth}")
+    #         if (l_depth == False) or (r_depth == False):
+    #             return False
+
+
+    #         if abs(l_depth - r_depth) > 1:
+    #             return False
+    #         return max(l_depth, r_depth)
+
+    #     result = depth(root)
+    #     if result:
+    #         return True
+    #     else:
+    #         return False
+
+    # below solution is from LEET example, I wasn't ablew to solve as per above
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """ 
+        if root == None:
+            return True
+        l = self.depth(root.left)
+        r = self.depth(root.right)
+        return (abs(l-r) <2) and self.isBalanced(root.left) and self.isBalanced(root.right)
+    
+    def depth(self,node):
+        if node == None: return 0
+        return max(self.depth(node.left),self.depth(node.right))+1
+
+
+root1 = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+Solution().isBalanced(root1) 
