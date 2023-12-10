e65f5cae3c2996be3fadb8471d26a48d7def3164
friendlyantz <70934030+friendlyantz@users.noreply.github.com>
2023-12-10 17:23:15 +1100

    Add zeitwerk and basic test

--- advent_of_code/2023/d01/solution_spec.rb
+++ advent_of_code/2023/d01/solution_spec.rb
@@ -1,7 +1,36 @@
-require File.join(__dir__, 'solution')
-
 require 'pry'
 require 'awesome_print'
+require 'zeitwerk'
+
+loader = Zeitwerk::Loader.new
+loader.push_dir(__dir__)
+loader.setup
+#
+# --- Day 1: Trebuchet?! ---
+#
+# Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+#
+# You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
+#
+# Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+#
+# You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+#
+# As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+#
+# The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+#
+# For example:
+#
+# 1abc2
+# pqr3stu8vwx
+# a1b2c3d4e5f
+# treb7uchet
+#
+# In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+#
+# Consider your entire calibration document. What is the sum of all of the calibration values?
+#
 
 RSpec.describe 'Solutions' do
   let(:example_input) { File.read(File.join(__dir__, 'ie_input')) }
@@ -12,8 +41,11 @@ RSpec.describe 'Solutions' do
     # DESCRIPTION Part 1
     #
     describe 'implementation' do
-      it 'description' do
-        expect(subject).to eq true
+      it 'calibration values are correct for each line' do
+        expect(LineCalibrator.new('1abc2').call).to eq 12
+        expect(LineCalibrator.new('pqr3stu8vwx').call).to eq 38
+        expect(LineCalibrator.new('a1b2c3d4e5f').call).to eq 15
+        expect(LineCalibrator.new('treb7uchet').call).to eq 77
       end
     end
 
@@ -21,7 +53,7 @@ RSpec.describe 'Solutions' do
       context 'example input data' do
         it 'returns correct result' do
           pending 'pt1 implementation'
-          expect(solution_pt1(example_input)).to eq true
+          expect(solution_pt1(example_input)).to eq 142
         end
       end
 
