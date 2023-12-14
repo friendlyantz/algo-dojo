f20bc22d7f23ea40d0075e458f98630fb9a638a9
friendlyantz <70934030+friendlyantz@users.noreply.github.com>
2023-12-14 21:19:57 +1100

    Add d2 desc. TDD RoundAssessor

---
+++ advent_of_code/2023/d02/round_assessor.rb
@@ -0,0 +1,21 @@
+# Description: Round assessor
+class RoundAssessor
+  LIMITS = {
+    red: 12,
+    green: 13,
+    blue: 14
+  }.freeze
+  def initialize(hash)
+    @red = hash[:red]
+    @green = hash[:green]
+    @blue = hash[:blue]
+  end
+
+  def call
+    return false if @red >= LIMITS[:red]
+    return false if @green >= LIMITS[:green]
+    return false if @blue >= LIMITS[:blue]
+
+    true
+  end
+end
