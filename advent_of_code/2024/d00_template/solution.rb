bb2bb800300e9c1e6ddd951f64936eec37448edb
friendlyantz <friendlyantz@pm.me>
2024-12-02 10:12:02 +1100

    Add 2024 AoC template

---
+++ advent_of_code/2024/d00_template/solution.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Solution
+  def initialize(input)
+    @input = input
+  end
+
+  def solution_pt1
+    # input
+  end
+
+  def solution_pt2
+    # input
+    # .then { |data| binding.pry }
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  if ARGV.empty?
+    raise 'please provide input file destination as an argument following script name when executing this file'
+  end
+
+  input = File.read(ARGV.first)
+  puts 'part 1 solution'
+  puts Solution.new(input).solution_pt1
+
+  puts '==============='
+  puts 'part 2 solution'
+  puts Solution.new(input).solution_pt2
+end
---
+++ advent_of_code/2024/d00_template/solution_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require File.join(__dir__, 'solution')
+
+require 'pry'
+require 'awesome_print'
+
+RSpec.describe 'Solutions' do
+  let(:example_input) { File.read(File.join(__dir__, 'data/ie_input')) }
+  let(:custom_input) { File.read(File.join(__dir__, 'data/input')) }
+
+  describe 'Part 1' do
+    #
+    # DESCRIPTION Part 1
+    #
+    describe 'implementation' do
+      it 'description' do
+        expect(subject).to eq true
+      end
+    end
+
+    describe 'final result for pt1' do
+      context 'with example input data' do
+        it 'returns correct result' do
+          pending 'pt1 implementation'
+          expect(Solution.new(example_input).solution_pt1).to eq true
+        end
+      end
+
+      context 'with custom input data' do
+        it 'returns correct result' do
+          pending 'pt1 implementation'
+          expect(Solution.new(custom_input).solution_pt1).to eq true
+        end
+      end
+    end
+  end
+
+  describe 'Part 2' do
+    #
+    # DESCRIPTION Part 2
+    #
+    context 'with example input data' do
+      it 'returns correct result' do
+        pending 'pt2 implementation'
+        expect(Solution.new(example_input).solution_pt2).to eq true
+      end
+    end
+
+    context 'with custom input data' do
+      it 'returns correct result' do
+        pending 'pt2 implementation'
+        expect(Solution.new(custom_input).solution_pt2).to eq true
+      end
+    end
+  end
+end
